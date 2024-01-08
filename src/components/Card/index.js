d8669cf2622e712e39745f1e7e49c638c8c66943
unknown <fandinofacundo123@gmail.com>
2024-01-08 19:44:20 -0300

    second commit

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Card(props){
     const {food} = props;
-    return <Link  className="linker" to={`/milanesas/${food.name}`}><div key={props.llave} className="cardGrapper">
+    return <Link  className="linker" to={`/comidas/${food.name}`}><div key={props.llave} className="cardGrapper">
     <h1 className="title">{food.name}</h1>
     <p className="text">{food.desc}</p>
 </div></Link>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./pages/App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Comidas from "./pages/Milanesas";
+import Comidas from "./pages/Comidas";
 import Manteca from "./pages/Manteca";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -12,7 +12,7 @@ root.render(
   <Router>
     <Routes>
       <Route path="/" element={<App />}/>
-      <Route path="/milanesas/:name" element={<Comidas/>}/>
+      <Route path="/comidas/:name" element={<Comidas/>}/>
       <Route path="/manteca" element={<Manteca/>}/>
     </Routes>
   </Router>
